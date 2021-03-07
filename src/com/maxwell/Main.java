5c0bdb63327d2b7bb68d7c6bbf5ae66fccf0e617
Maxwell <maxwell.guru.1@gmail.com>
2021-03-07 13:57:13 -0700

    initial commit

---
+++ src/com/maxwell/Main.java
@@ -0,0 +1,30 @@
+package com.maxwell;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public static void main(String[] args) {
+
+        Scanner myObj1 = new Scanner(System.in);
+        System.out.println("Hey youre finally awake");
+
+        System.out.println("What do you do?");
+        System.out.println("1: Talk to rolf");
+        System.out.println("2: Jump from wagon");
+
+        //int _1 = myObj1.Scanner
+//        System.out.println("-You were trying to cross the border, walked right into that Imperial ambush, same as us and that thief over there.");
+        awaken();
+
+
+
+    }
+
+    public  static void awaken() {
+        System.out.println("-You were trying to cross the border, walked right into that Imperial ambush, same as us and that thief over there.");
+
+    }
+}
